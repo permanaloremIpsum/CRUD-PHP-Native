3c5d20a7848d82cf39bba54624d770bac29485d4
permanaloremIpsum <dpermana350@gmail.com>
2020-02-24 08:30:41 +0700

    Menambahkan Cookie

--- login.php
+++ login.php
@@ -7,6 +7,26 @@ if(isset($_SESSION["login"])){
 }
 require 'functions.php';
 
+// cek cookie
+if(isset($_COOKIE['id']) && isset($_COOKIE['key'])){
+	$id = $_COOKIE['id'];
+	$key = $_COOKIE['key'];
+
+	//ambil username berdasarkan id
+	$result = mysqli_query($db, "SELECT username FROM user WHERE id = '$id'");
+	$row = mysqli_fetch_assoc($result);
+
+	// cek cookie dan username
+	if($key === hash('sha256', $row["username"])){
+		$_SESSION['login'] = true;
+	}
+}
+
+if(isset($_SESSION["login"])){
+	header("Location: index.php");
+	exit;
+}
+
 if(isset($_POST["login"])){
 	$username = $_POST["username"];
 	$password = $_POST["password"];
@@ -20,6 +40,14 @@ if(isset($_POST["login"])){
 		if(password_verify($password, $row["password"])){
 			// set session
 			$_SESSION["login"] = true;
+
+			// cek remember me
+			if(isset($_POST["remember"])){
+				//buat cookie
+
+				setcookie('id', $row['id'], time()+300);
+				setcookie('key', hash('sha256', $row['username']), time()+300);
+			}
 
 			header("Location: index.php");
 			exit;
